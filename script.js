4998e424c8cbaec02eae636958992fd48ce73fee
unknown <sa.vahidinovin@gmail.com>
2024-07-11 14:43:14 +0200

    exercise1-2

---
+++ script.js
@@ -0,0 +1,8 @@
+//1. What is the name of the recipe?
+const recipeNameRef = document.querySelector('.description-container h1').innerHTML;
+console.log(recipeNameRef);
+
+//2. What HTML tag is used to display the Recipe name?
+const recipeNameElement = document.getElementById('recipe-name');
+console.log(recipeNameElement);
+
