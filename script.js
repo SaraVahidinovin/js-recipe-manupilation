9db084dfaf175b0e975a541a8f3f6f01e51e1270
unknown <sa.vahidinovin@gmail.com>
2024-07-11 16:49:00 +0200

    exercise3-5

--- script.js
+++ script.js
@@ -6,3 +6,33 @@ console.log(recipeNameRef);
 const recipeNameElement = document.getElementById('recipe-name');
 console.log(recipeNameElement);
 
+//3. What is the font size of the paragraph tag with the class "description".
+const descriptionRef = document.querySelector('.description');
+const fontDes = window.getComputedStyle(descriptionRef).fontSize;
+console.log(fontDes);
+
+//4. What is the value of the alt atrribute on the image?
+const imgRef = document.querySelector('.image-container img');
+const imgAlt = imgRef.getAttribute('alt');  //  const imageAlt = imgRef.alt;
+console.log(imgAlt);
+
+/*5. What is the dimensions and the url of the image? Create an object that looks like this, and log it to the console:
+{
+  url: string
+  height: number,
+  width: number,
+} */
+
+const imgUrl = imgRef.src; //const srcRef = imgRef.getAttribute('src');
+const imgHeight = imgRef.offsetHeight;
+const imgWidth = imgRef.offsetWidth;
+
+const imageDetails = {
+    url: imgUrl,
+    height: imgHeight,
+    width: imgWidth
+};
+
+console.log(imageDetails);
+
+
