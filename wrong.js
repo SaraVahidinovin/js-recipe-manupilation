00210b603cad238dd082e7ccdf42842ac758d25e
unknown <sa.vahidinovin@gmail.com>
2024-07-12 11:16:37 +0200

    exercise1

---
+++ wrong.js
@@ -0,0 +1,5 @@
+// 1. The logo text of the site has the wrong color. Change it to the correct one.
+const logoRef = document.querySelector('.logo-text');
+logoRef.style.color = "#384241";
+
+//2. The alignment of the elements inside the header element are wrong. 
